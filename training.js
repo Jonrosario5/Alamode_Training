449a18e84fc61f4e670a5a645f4621f6c9005f29
Jon Rosario <41496659+Jonrosario5@users.noreply.github.com>
2021-08-19 08:54:32 -0700

    Update training.js

--- training.js
+++ training.js
@@ -8,6 +8,12 @@ test2:function(){
  
    $("#jon").html(`<iframe src="https://giphy.com/embed/demgpwJ6rs2DS" width="480" height="359" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/perfectloops-chef-melons-demgpwJ6rs2DS">via GIPHY</a></p>`)
 
+},
+  
+test3:function(name){
+
+  $(`<h1>${name} is an Alamode Master Now`)
+
 }
 
 }
