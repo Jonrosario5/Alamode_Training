841bc3e4ee9c15275ebc99c31ed7176c56057a22
Jon Rosario <41496659+Jonrosario5@users.noreply.github.com>
2021-08-19 08:45:50 -0700

    Update training.js

--- training.js
+++ training.js
@@ -6,7 +6,7 @@ test1:function(){
   
 test2:function(){
  
-   $("#jon").html(`<iframe src="https://gfycat.com/feminineillfatedgar"></iframe>`)
+   $("#jon").html(`<iframe src="https://giphy.com/embed/demgpwJ6rs2DS" width="480" height="359" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/perfectloops-chef-melons-demgpwJ6rs2DS">via GIPHY</a></p>`)
 
 }
 
